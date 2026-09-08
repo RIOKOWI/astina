19cdd3a350e8520f4e3db2541b13c4002a7468d3
RIOKOWI <achyarrio2004@gmail.com>
2026-09-09 01:51:46 +0700

    refactor(core): update router refresh notifier

--- lib/core/services/router_refresh_notifier.dart
+++ lib/core/services/router_refresh_notifier.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../features/auth/providers/auth_provider.dart';
+import 'auth_state.dart';
 
 final routerRefreshNotifier = _RouterRefreshNotifier();
 
