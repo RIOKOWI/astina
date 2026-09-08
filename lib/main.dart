7bf50ce40599b866dac445eef6b5e922ea67ac04
RIOKOWI <achyarrio2004@gmail.com>
2026-09-09 01:53:38 +0700

    refactor(sos): remove sos provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 import 'core/services/fcm_service.dart';
 
-final fcmService = FCMService.instance;
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await Firebase.initializeApp();
-  await fcmService.initialize();
+  await FCMService.instance.initialize();
   runApp(const ProviderScope(child: AstinaApp()));
 }
