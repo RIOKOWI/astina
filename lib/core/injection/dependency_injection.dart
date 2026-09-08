68c68af94c4d52cefd87b6d797d6ba4ff018c0c7
RIOKOWI <achyarrio2004@gmail.com>
2026-09-09 01:51:41 +0700

    refactor(core): update dependency injection

--- lib/core/injection/dependency_injection.dart
+++ lib/core/injection/dependency_injection.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/dio_client.dart';
 import '../services/secure_storage.dart';
+import '../../features/auth/data/datasources/auth_remote_data_source.dart';
 
 final secureStorageProvider = Provider<SecureStorageService>((ref) {
   return SecureStorageService();
@@ -9,4 +10,9 @@ final secureStorageProvider = Provider<SecureStorageService>((ref) {
 final dioClientProvider = Provider<DioClient>((ref) {
   final storage = ref.watch(secureStorageProvider);
   return DioClient(storage);
+});
+
+final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
+  final dioClient = ref.watch(dioClientProvider);
+  return AuthRemoteDataSource(dioClient);
 });
