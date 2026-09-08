a5abe1702dc87269d5bf54c63e6c80a9db0144fe
RIOKOWI <achyarrio2004@gmail.com>
2026-09-09 01:52:10 +0700

    refactor(complaint): remove complaint attachment provider

--- lib/features/complaint/providers/complaint_attachment_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/injection/dependency_injection.dart';
-import '../data/datasources/complaint_remote_data_source.dart';
-
-final complaintAttachmentRemoteDataSourceProvider =
-    Provider<ComplaintRemoteDataSource>((ref) {
-      final dioClient = ref.watch(dioClientProvider);
-      return ComplaintRemoteDataSource(dioClient);
-    });
