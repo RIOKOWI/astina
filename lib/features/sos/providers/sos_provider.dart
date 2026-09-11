98e3af3782ae36ac4c4a77dc1da2c724721a7e37
RIOKOWI <achyarrio2004@gmail.com>
2026-09-11 20:38:02 +0700

    feat(sos): add sos notifier for triggering alert and audio

--- lib/features/sos/providers/sos_provider.dart
+++ lib/features/sos/providers/sos_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/injection/dependency_injection.dart';
 import '../data/datasources/sos_remote_data_source.dart';
 import '../data/models/sos_alert_model.dart';
+import '../services/sos_audio_service.dart';
 
 final sosRemoteDataSourceProvider = Provider<SosRemoteDataSource>((ref) {
   return SosRemoteDataSource(ref.watch(dioClientProvider));
@@ -19,3 +20,32 @@ final sosAlertDetailProvider = FutureProvider.autoDispose
       final ds = ref.watch(sosRemoteDataSourceProvider);
       return ds.getAlert(id);
     });
+
+class SosNotifier extends Notifier<void> {
+  @override
+  void build() {}
+
+  Future<SosAlertModel> triggerSos({
+    required double latitude,
+    required double longitude,
+    String? locationText,
+  }) async {
+    final ds = ref.read(sosRemoteDataSourceProvider);
+    final alert = await ds.createAlert(
+      latitude: latitude,
+      longitude: longitude,
+      locationText: locationText,
+    );
+    SosAudioService.instance.playSiren();
+    ref.invalidate(sosActiveAlertsProvider);
+    return alert;
+  }
+
+  void stopSiren() {
+    SosAudioService.instance.stopSiren();
+  }
+}
+
+final sosNotifierProvider = NotifierProvider<SosNotifier, void>(
+  SosNotifier.new,
+);
