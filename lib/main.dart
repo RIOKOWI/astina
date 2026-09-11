4aaaccd530d3065daecc0bdc6626c800d015848e
RIOKOWI <achyarrio2004@gmail.com>
2026-09-11 20:38:00 +0700

    feat(main): listen to sos alert fcm message to play siren

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 import 'core/services/fcm_service.dart';
+import 'features/sos/services/sos_audio_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await Firebase.initializeApp();
   await FCMService.instance.initialize();
+
+  fcmMessageStream.listen((message) {
+    final type = message.data['type'] as String?;
+    if (type == 'sos_alert') {
+      SosAudioService.instance.playSiren();
+    }
+  });
+
   runApp(const ProviderScope(child: AstinaApp()));
 }
