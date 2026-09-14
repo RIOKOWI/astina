dfb27777e2709ace8282dcfd5e0740666367aba6
RIOKOWI <achyarrio2004@gmail.com>
2026-09-14 23:45:53 +0700

    refactor(android): extend flutterfragmentactivity

--- android/app/src/main/kotlin/com/example/astina/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/astina/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.astina
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         createNotificationChannel()
