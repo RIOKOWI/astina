e06208f5b43b7ed4a26c6220bb707db58b1b3918
RIOKOWI <achyarrio2004@gmail.com>
2026-09-16 15:50:59 +0700

    feat(di): register biometric service provider

--- lib/core/injection/dependency_injection.dart
+++ lib/core/injection/dependency_injection.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/dio_client.dart';
 import '../services/secure_storage.dart';
+import '../services/biometric_service.dart';
 import '../../features/auth/data/datasources/auth_remote_data_source.dart';
 
 final secureStorageProvider = Provider<SecureStorageService>((ref) {
@@ -15,4 +16,8 @@ final dioClientProvider = Provider<DioClient>((ref) {
 final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
   final dioClient = ref.watch(dioClientProvider);
   return AuthRemoteDataSource(dioClient);
+});
+
+final biometricServiceProvider = Provider<BiometricService>((ref) {
+  return BiometricService();
 });
