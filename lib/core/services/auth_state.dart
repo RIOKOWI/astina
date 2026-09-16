ff5a607eb909b8b4473abca560f46d2e26ae96f6
RIOKOWI <achyarrio2004@gmail.com>
2026-09-16 13:35:29 +0700

    feat(auth): add loading state to auth state service

--- lib/core/services/auth_state.dart
+++ lib/core/services/auth_state.dart
@@ -3,10 +3,15 @@ import '../../features/auth/data/models/user_model.dart';
 sealed class AuthState {
   const AuthState();
   bool get isAuthenticated => this is Authenticated;
+  bool get isLoading => this is AuthLoading;
   UserModel? get user => switch (this) {
     Authenticated(:final user) => user,
     _ => null,
   };
+}
+
+class AuthLoading extends AuthState {
+  const AuthLoading();
 }
 
 class Authenticated extends AuthState {
