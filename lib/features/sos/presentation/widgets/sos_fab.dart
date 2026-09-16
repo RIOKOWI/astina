df39d14fab1f98fe82ea18e681bd8b087142ba3c
RIOKOWI <achyarrio2004@gmail.com>
2026-09-16 19:28:02 +0700

    refactor(sos): update geolocator method arguments

--- lib/features/sos/presentation/widgets/sos_fab.dart
+++ lib/features/sos/presentation/widgets/sos_fab.dart
@@ -125,10 +125,8 @@ class SosFab extends ConsumerWidget {
 
     try {
       final position = await Geolocator.getCurrentPosition(
-        locationSettings: const LocationSettings(
-          accuracy: LocationAccuracy.high,
-          timeLimit: Duration(seconds: 10),
-        ),
+        desiredAccuracy: LocationAccuracy.high,
+        timeLimit: const Duration(seconds: 10),
       );
 
       final alert = await ref
